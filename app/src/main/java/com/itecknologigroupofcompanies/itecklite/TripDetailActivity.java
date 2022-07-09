7250246c5519b5bbc457ea3db3f3e507f9d14caa
Rao Aaraf Younus <aarafrao22@gmail.com>
2022-07-09 18:36:47 +0500

    Final

--- app/src/main/java/com/itecknologigroupofcompanies/itecklite/TripDetailActivity.java
+++ app/src/main/java/com/itecknologigroupofcompanies/itecklite/TripDetailActivity.java
@@ -48,6 +48,7 @@ import com.google.firebase.messaging.FirebaseMessaging;
 
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
 
@@ -62,7 +63,7 @@ public class TripDetailActivity extends AppCompatActivity implements OnMapReadyC
     private GoogleMap mMap;
     private BottomSheetBehavior bottomSheetBehavior;
     ConstraintLayout layout;
-    TextView txt_time;
+    TextView txt_time, txtSpeed, txtIgnition, txtLatLong, txtVehicleNo;
 
     private RecyclerView recyclerView;
     TripAdapter adapter;
@@ -89,6 +90,15 @@ public class TripDetailActivity extends AppCompatActivity implements OnMapReadyC
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_trip_detail);
 
+        loadingDialogue = new Dialog(this);
+        loadingDialogue.setContentView(R.layout.loading);
+        loadingDialogue.getWindow().setBackgroundDrawable(getDrawable(R.drawable.rounded_corner_main_activity));
+        loadingDialogue.getWindow().setLayout(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+        loadingDialogue.setCancelable(false);
+
+
+        loadingDialogue.show();
+
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
 
@@ -98,13 +108,13 @@ public class TripDetailActivity extends AppCompatActivity implements OnMapReadyC
 
         notificationWalaKaam();
         getToken();
-        loadingDialogue = new Dialog(this);
-        loadingDialogue.setContentView(R.layout.loading);
-        loadingDialogue.getWindow().setBackgroundDrawable(getDrawable(R.drawable.rounded_corner_main_activity));
-        loadingDialogue.getWindow().setLayout(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
-        loadingDialogue.setCancelable(false);
+
 
         layout = findViewById(R.id.txtNumP);
+        txtSpeed = findViewById(R.id.txtSpeed);
+        txtVehicleNo = findViewById(R.id.txtSelectedVehicle);
+        txtLatLong = findViewById(R.id.txtLatLong);
+        txtIgnition = findViewById(R.id.txtIgnition);
         txt_time = findViewById(R.id.txt_time);
         txtDate = findViewById(R.id.txt_date);
         txtVehicleDetails = findViewById(R.id.txtVehicleDetails);
@@ -114,45 +124,24 @@ public class TripDetailActivity extends AppCompatActivity implements OnMapReadyC
 
         ConstraintLayout bottomSheetLayout = findViewById(R.id.bottom_sheet);
         bottomSheetBehavior = BottomSheetBehavior.from(bottomSheetLayout);
-        bottomSheetBehavior.setPeekHeight(420);
-
-        bottomSheetBehavior.setBottomSheetCallback(new BottomSheetBehavior.BottomSheetCallback() {
-            @Override
-            public void onStateChanged(@NonNull View bottomSheet, int newState) {
-                switch (newState) {
-                    case BottomSheetBehavior.STATE_HIDDEN:
-//                        Toast.makeText(TripDetailActivity.this, "STATE HIDDEN", Toast.LENGTH_SHORT).show();
-                        break;
-                    case BottomSheetBehavior.STATE_EXPANDED:
-//                        Toast.makeText(TripDetailActivity.this, "STATE Exp", Toast.LENGTH_SHORT).show();
-
-                        break;
-                    case BottomSheetBehavior.STATE_COLLAPSED:
-//                        Toast.makeText(TripDetailActivity.this, "STATE Collapsed", Toast.LENGTH_SHORT).show();
-
-                }
-            }
-
-            @Override
-            public void onSlide(@NonNull View bottomSheet, float slideOffset) {
-
-            }
-        });
+        bottomSheetBehavior.setPeekHeight(400);
 
         try {
-//            getCarData(phNo);
-            getCarData("03092225699");
+            getCarData(phNo);
+            loadingDialogue.dismiss();
         } catch (Exception e) {
-
+            loadingDialogue.dismiss();
+            Toast.makeText(getApplicationContext(), "Something Went Wrong", Toast.LENGTH_SHORT).show();
         }
-        rvInitialization();
 
-        listData();
+//        rvInitialization();
+//
+//        listData();
 
     }
 
     private void getCarData(String contactNo) {
-//        loadingDialogue.show();
+        loadingDialogue.show();
 
         Retrofit retrofit = new Retrofit.Builder()
                 .baseUrl("https://iot.itecknologi.com/mobile/loadcustomerdata.php/")
@@ -176,13 +165,21 @@ public class TripDetailActivity extends AppCompatActivity implements OnMapReadyC
                 }
 
                 String selectedCarVid = VehicleId.get(0);
-                getSelectedCarData(VehicleId.get(0));
+
+                try {
+                    getSelectedCarData(VehicleId.get(0));
+                } catch (Exception e) {
+                    Toast.makeText(TripDetailActivity.this, "Something Went Wrong", Toast.LENGTH_SHORT).show();
+                }
                 txtNoCar.setText(VehicleRegId.get(0).toString());
+
+                loadingDialogue.dismiss();
 
             }
 
             @Override
             public void onFailure(Call<ResponseModel> call, Throwable t) {
+                loadingDialogue.dismiss();
                 Toast.makeText(getApplicationContext(), t.toString(), Toast.LENGTH_SHORT).show();
             }
         });
@@ -234,37 +231,37 @@ public class TripDetailActivity extends AppCompatActivity implements OnMapReadyC
                 });
     }
 
-    private void rvInitialization() {
-        recyclerView = findViewById(R.id.RecyclerView);
-        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
-        layoutManager.setOrientation(RecyclerView.VERTICAL);
-        recyclerView.setLayoutManager(layoutManager);
+//    private void rvInitialization() {
+//        recyclerView = findViewById(R.id.RecyclerView);
+//        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+//        layoutManager.setOrientation(RecyclerView.VERTICAL);
+//        recyclerView.setLayoutManager(layoutManager);
+//
+//        list = new ArrayList<>();
+//        adapter = new TripAdapter(list);
+//        recyclerView.setAdapter(adapter);
+//    }
 
-        list = new ArrayList<>();
-        adapter = new TripAdapter(list);
-        recyclerView.setAdapter(adapter);
-    }
-
-    private void listData() {
-        list.add(new TripModel("2:54pm", "9:10pm", "iTecknologi Group of Companies"
-                , "Department of Computer Science UBIT", "21 KM", "3:01pm"));
-
-        list.add(new TripModel("1:22pm", "10:01pm", "Clifton BLock 7 Mai Kolachi Bypass"
-                , "University Of Karachi", "20 KM", "1:01am"));
-
-        list.add(new TripModel("2:54pm", "9:10pm", "Nandos Clifton Opp. Shaheed Benazir Bhutto Park"
-                , "Department of Computer Science UBIT", "21 KM", "3:01pm"));
-
-        list.add(new TripModel("2:54pm", "9:10pm", "iTecknologi Group of Companies"
-                , "Department of Computer Science UBIT", "21 KM", "3:01pm"));
-
-        list.add(new TripModel("2:54pm", "9:10pm", "iTecknologi Group of Companies"
-                , "Department of Computer Science UBIT", "21 KM", "3:01pm"));
-
-        list.add(new TripModel("2:54pm", "9:10pm", "iTecknologi Group of Companies"
-                , "Department of Computer Science UBIT", "21 KM", "3:01pm"));
-
-    }
+//    private void listData() {
+//        list.add(new TripModel("2:54pm", "9:10pm", "iTecknologi Group of Companies"
+//                , "Department of Computer Science UBIT", "21 KM", "3:01pm"));
+//
+//        list.add(new TripModel("1:22pm", "10:01pm", "Clifton BLock 7 Mai Kolachi Bypass"
+//                , "University Of Karachi", "20 KM", "1:01am"));
+//
+//        list.add(new TripModel("2:54pm", "9:10pm", "Nandos Clifton Opp. Shaheed Benazir Bhutto Park"
+//                , "Department of Computer Science UBIT", "21 KM", "3:01pm"));
+//
+//        list.add(new TripModel("2:54pm", "9:10pm", "iTecknologi Group of Companies"
+//                , "Department of Computer Science UBIT", "21 KM", "3:01pm"));
+//
+//        list.add(new TripModel("2:54pm", "9:10pm", "iTecknologi Group of Companies"
+//                , "Department of Computer Science UBIT", "21 KM", "3:01pm"));
+//
+//        list.add(new TripModel("2:54pm", "9:10pm", "iTecknologi Group of Companies"
+//                , "Department of Computer Science UBIT", "21 KM", "3:01pm"));
+//
+//    }
 
     @Override
     public void onMapReady(@NonNull GoogleMap googleMap) {
@@ -296,7 +293,12 @@ public class TripDetailActivity extends AppCompatActivity implements OnMapReadyC
             public void onClick(DialogInterface dialog, int item) {
                 txtNoCar.setText(VehicleRegId.get(item).toString());
                 String selectedCarVid = VehicleId.get(item);
-                getSelectedCarData(selectedCarVid);
+
+                try {
+                    getSelectedCarData(selectedCarVid);
+                } catch (Exception e) {
+                    Toast.makeText(TripDetailActivity.this, "Something Went Wrong", Toast.LENGTH_SHORT).show();
+                }
 
                 dialog.dismiss();
             }
@@ -306,7 +308,7 @@ public class TripDetailActivity extends AppCompatActivity implements OnMapReadyC
     }
 
     private void getSelectedCarData(String selectedCarVid) {
-//        loadingDialogue.show();
+        loadingDialogue.show();
 
         Retrofit retrofit = new Retrofit.Builder()
                 .baseUrl("https://iot.itecknologi.com/mobile/get_vehicle_latest_info.php/")
@@ -332,11 +334,24 @@ public class TripDetailActivity extends AppCompatActivity implements OnMapReadyC
                 GpsTime gpsTime = response.body().GpsTime;
                 String V_Ang = response.body().getV_Ang();
 
+                txtSpeed.setText(speed);
+                txtVehicleNo.setText(vehicleNo);
+                String ign;
+                if (Integer.parseInt(ignition) == 0) {
+                    ign = "Ignition OFF";
+                } else {
+                    ign = "Ignition ON";
+                }
+                txtIgnition.setText(ign);
+                txtLatLong.setText(x + " , " + y);
                 txtVehicleDetails.setText("Last Reported Location of your vehicle is\n" + location);
                 String string = gpsTime.date;
                 String[] parts = string.split(" ");
                 String date = parts[0]; // 004
                 String time = parts[1];
+
+                String[] dayyyy = date.split("-");
+                String vDay = dayyyy[2];
 
                 txtDate.setText(date);
                 locationX = Double.parseDouble(x);
@@ -368,23 +383,30 @@ public class TripDetailActivity extends AppCompatActivity implements OnMapReadyC
                 String[] tmp1 = s.split(":");
                 int currentTime = Integer.parseInt(tmp1[0]);
 
-                if (hour > currentTime) {
+
+                @SuppressLint("SimpleDateFormat")
+                String currentDay = new SimpleDateFormat("dd").format(Calendar.getInstance().getTime());
+                int intDay = Integer.parseInt(vDay);
+
+
+                if (hour > currentTime && intDay > Integer.parseInt(currentDay)) {
                     vehicleColor = R.drawable.gray_car;
-                } else if (ignition.equals("1") || speed.equals("0")) {
-                    vehicleColor = R.drawable.green_car;
-                } else if (ignition.equals("0")) {
+                } else if (Integer.parseInt(ignition) == 0) {
                     vehicleColor = R.drawable.red_car;
+                } else if (Integer.parseInt(ignition) == 1 || speed.equals("0")) {
+                    vehicleColor = R.drawable.green_car;
                 } else {
                     vehicleColor = R.drawable.black_car;
                 }
 
                 onMapReady(mMap);
-
+                loadingDialogue.dismiss();
             }
 
             @Override
             public void onFailure(Call<SelectedVehicleResponseModel> call, Throwable t) {
-                Toast.makeText(getApplicationContext(), t.toString(), Toast.LENGTH_SHORT).show();
+                Toast.makeText(getApplicationContext(), "API Error", Toast.LENGTH_SHORT).show();
+                loadingDialogue.dismiss();
             }
         });
     }
